da4ad490efba876217e32442554e2e51b0e3b712
TomsenTan <36121978+TomsenTan@users.noreply.github.com>
2019-03-11 20:24:05 +0800

    Update celery_runner.py

--- celery_runner.py
+++ celery_runner.py
@@ -19,7 +19,7 @@ def make_celery(app):
      celery = Celery(
          app.import_name,
          broker = app.config['CELERY_BROKER_URL'],
-         backend=app.config['CELERY_BACKEND_URL']
+         backend = app.config['CELERY_BACKEND_URL']
      )
 
      celery.conf.update(app.config)
